49e0ae721845e7de80e6f879aad82727d6871ca4
cwi.allan <cwi.allan@agibank.com.br>
2018-08-10 18:37:27 -0300

    adicionado watcher

--- src/main/java/br/com/agibank/app/Executer.java
+++ src/main/java/br/com/agibank/app/Executer.java
@@ -1,11 +1,30 @@
 package br.com.agibank.app;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class Executer {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new Executer().getGreeting());
+
+        try {
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+            Path path = Paths.get("in");
+
+            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+
+            WatchKey key;
+            while ((key = watchService.take()) != null) {
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    System.out.println("Event kind:" + event.kind() + ". File affected: " + event.context() + ".");
+                }
+                key.reset();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
