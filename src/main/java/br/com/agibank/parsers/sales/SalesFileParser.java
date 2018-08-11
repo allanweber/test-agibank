e6a7e4a1181e238718c295ae31788243e158dfea
Allan Weber <a.cassianoweber@gmail.com>
2018-08-11 16:25:07 -0300

    Classe agrupando as informações

--- src/main/java/br/com/agibank/parsers/sales/SalesFileParser.java
+++ src/main/java/br/com/agibank/parsers/sales/SalesFileParser.java
@@ -1,38 +1,43 @@
 package br.com.agibank.parsers.sales;
 
+import br.com.agibank.model.sales.FullSaleFile;
 import br.com.agibank.parsers.IFileParser;
 import br.com.agibank.utilities.Console;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class SalesFileParser implements IFileParser {
 
-    private List<String> dataTypes = Arrays.asList("001", "002", "003");
+    private FullSaleFile file;
+
+    public FullSaleFile getFullSaleFile(){
+        return  file;
+    }
 
     @Override
     public void parseFile(List<String> lines) {
 
+        file = new FullSaleFile();
+
         try {
             for (String line : lines) {
-                validateLineDateType(line);
 
-
+                if(line.startsWith(Constants.SALESMANTYPE)){
+                    file.Salesmen.add(SalesmanParser.parse(line));
+                } else if(line.startsWith(Constants.CUSTOMERTYPE)){
+                    file.Customers.add(CustomerParser.parse(line));
+                } else if(line.startsWith(Constants.SALETYPE)){
+                    file.Sales.add(SaleParser.parse(line));
+                } else{
+                    String message = "Data type of this line is not valid";
+                    Console.LogError(message + ": " + line);
+                    throw new Exception(message);
+                }
 
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-    }
-
-    private void validateLineDateType(String line) throws Exception {
-        String message = "Data type of this line is not valid";
-        for (String type : dataTypes) {
-            if (!line.startsWith(type)) {
-                Console.LogError(message + ": " + line);
-                throw new Exception(message);
-            }
-        }
     }
 }
---
+++ src/test/java/br/com/agibank/parsers/SalesFileParserTest.java
@@ -0,0 +1,50 @@
+package br.com.agibank.parsers;
+
+import br.com.agibank.model.sales.FullSaleFile;
+import br.com.agibank.model.sales.Sale;
+import br.com.agibank.parsers.sales.SalesFileParser;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SalesFileParserTest {
+
+    @Test
+    public void ShouldParseFileThrowsException() {
+        String message = "Data type of this line is not valid";
+        SalesFileParser parser = new SalesFileParser();
+        try {
+            parser.parseFile(Arrays.asList("004ç1234567891234çDiegoç50000"));
+        } catch (Exception e) {
+            assertTrue("Message should be" + message, e.getMessage().equals(message));
+        }
+    }
+
+    @Test
+    public void ShouldParseFileSucceed() {
+
+        List<String> lines =
+                Arrays.asList(
+                        "001ç1234567891234çDiegoç50000",
+                        "001ç3245678865434çRenatoç40000.99",
+                        "002ç2345675434544345çJose da SilvaçRural",
+                        "002ç2345675433444345çEduardo PereiraçRural",
+                        "003ç10ç[1-10-100,2-30-2.50,3-40-3.10]çDiego",
+                        "003ç08ç[1-34-10,2-33-1.50,3-40-0.10]çRenato");
+
+        SalesFileParser parser = new SalesFileParser();
+        parser.parseFile(lines);
+
+        FullSaleFile file  = parser.getFullSaleFile();
+
+        assertTrue(file.getSalesmen().size() == 2);
+        assertTrue(file.getCustomers().size() == 2);
+        assertTrue(file.getSales().size() == 2);
+
+        Sale sale = file.getSales().get(0);
+        assertTrue(sale.getItens().size() == 3);
+    }
+}
