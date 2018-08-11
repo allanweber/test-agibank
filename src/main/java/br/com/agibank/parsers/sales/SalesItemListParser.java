9cd604959b87181dc15c4e67ea4e64c6c2565849
Allan Weber <a.cassianoweber@gmail.com>
2018-08-11 15:57:00 -0300

    Parser da lista de itens

---
+++ src/main/java/br/com/agibank/parsers/sales/SalesItemListParser.java
@@ -0,0 +1,24 @@
+package br.com.agibank.parsers.sales;
+
+import br.com.agibank.model.sales.SaleItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SalesItemListParser {
+
+    private static final String SEPARATOR = ",";
+
+    public static List<SaleItem> parse(String line) {
+
+        List<SaleItem> itens = new ArrayList<>();
+
+        String[] data = line.split(SEPARATOR);
+
+        for (String item: data){
+            itens.add(SaleItemParser.parse(item));
+        }
+
+        return itens;
+    }
+}
